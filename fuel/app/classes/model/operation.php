1b1828ee622186bff5273731818bb97a28c09cfa
alex-baiet <alex.baiet3@gmail.com>
2021-12-02 16:13:03 +0100

    Encore plus de syntaxe fixé

--- fuel/app/classes/model/operation.php
+++ fuel/app/classes/model/operation.php
@@ -1,27 +1,27 @@
 <?php
 
 class Model_Operation extends Orm\Model{
-  protected static $_properties = array(
-    'id_site',
-    'id_user',
-    'nom_op',
-    'a_revoir',
-    'annee',
-    'id_commune',
-    'adresse',
-    'X',
-    'Y',
-    'id_organisme',
-    'id_type_op',
-    'EA',
-    'OA',
-    'patriarche',
-    'numero_operation',
-    'arrete_prescription',
-    'responsable_op',
-    'anthropologue',
-    'paleopathologiste',
-    'etude_paleopathologique',
-    'bibliographie'
-  );
+	protected static $_properties = array(
+		'id_site',
+		'id_user',
+		'nom_op',
+		'a_revoir',
+		'annee',
+		'id_commune',
+		'adresse',
+		'X',
+		'Y',
+		'id_organisme',
+		'id_type_op',
+		'EA',
+		'OA',
+		'patriarche',
+		'numero_operation',
+		'arrete_prescription',
+		'responsable_op',
+		'anthropologue',
+		'paleopathologiste',
+		'etude_paleopathologique',
+		'bibliographie'
+	);
 }
