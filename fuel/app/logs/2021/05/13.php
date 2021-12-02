91f68eb41c13f31613abed20a4fc030f2533621f
alex-baiet <alex.baiet3@gmail.com>
2021-12-02 15:39:39 +0100

    Encore un trick

--- fuel/app/logs/2021/05/13.php
+++
@@ -1,4 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-ERROR - 2021-05-13 15:25:30 --> Error - Too few arguments to function alertBootstrap(), 1 passed in /sites/archeohandi/www/web_main/fuel/app/views/add/operation.php on line 270 and exactly 2 expected in /sites/archeohandi/www/web_main/fuel/app/views/add/operation.php on line 288
-ERROR - 2021-05-13 16:30:15 --> Warning - array_push() expects parameter 1 to be array, null given in /sites/archeohandi/www/web_main/fuel/app/classes/controller/add.php on line 776
--- fuel/app/logs/2021/07/29.php
+++
@@ -1,3 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-ERROR - 2021-07-29 18:01:55 --> 42000 - SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '' at line 1 with query: "SELECT type_pathologie FROM pathologie WHERE id_pathologie=" in /sites/archeohandi/www/web_main/fuel/core/classes/database/pdo/connection.php on line 235
