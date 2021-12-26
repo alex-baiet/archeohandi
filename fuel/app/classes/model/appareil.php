f2a88a615b61c9bd85e534abe145e9d72bb03cdc
alex-baiet <alex.baiet3@gmail.com>
2021-12-26 11:16:47 +0100

    Ajout model Appareil

---
+++ fuel/app/classes/model/appareil.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Model;
+
+use Fuel\Core\Model;
+
+/** Représente un appareil compensatoire pour un sujet handicapé. */
+class Appareil extends Model {
+	private int $id;
+	private string $nom;
+
+	public function __construct(array $values) {
+		$this->id = Helper::arrayGetInt("id_appareil_compensatoire", $values);
+		$this->nom = Helper::arrayGetString("type_appareil", $values);
+	}
+
+	/**
+	 * Retourne l'appareil correspondant à l'id donné.
+	 * @param int $id Identifiant.
+	 * @return Appareil|null Le résultat est null si aucun appareil ne correspond à l'id donné.
+	 */
+	public static function fetchSingle(int $id) {
+		if (!is_numeric($id)) return null;
+
+		$res = Helper::querySelectSingle("SELECT * FROM appareil_compensatoire WHERE id_appareil_compensatoire=$id;");
+		if ($res === null) return null;
+		return new Appareil($res);
+	}
+
+	/**
+	 * Retourne tous les appareils.
+	 * @return Appareil[]
+	 */
+	public static function fetchAll() {
+		$results = Helper::querySelect("SELECT * FROM appareil_compensatoire;");
+		$objects = array();
+
+		foreach ($results as $res) {
+			$objects[] = new Appareil($res);
+		}
+		return $objects;
+	}
+
+	public function getId() { return $this->id; }
+	public function getNom() { return $this->nom; }
+}
