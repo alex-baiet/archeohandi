7a29bf6a1b14cd99112ddb42ddee72bd039d8741
alex-baiet <alex.baiet3@gmail.com>
2022-02-09 08:23:48 +0100

    Ajout vérification en temps réel de sujet/form

--- fuel/app/views/sujet/form.php
+++ fuel/app/views/sujet/form.php
@@ -49,259 +49,262 @@ Form::open(array(
 	<input type="hidden" name="id_operation" value="<?= $idOperation ?>">
 <?php endif; ?>
 <?php if ($subject->getId() !== null) echo Form::hidden("id", $subject->getId()); ?>
-<div class="col-auto">
-	<h2 class="text-center">Groupe du sujet</h2>
+<h2 class="text-center">Groupe du sujet</h2>
 
-	<div class="row g-2">
-		<?php $group = $subject->getGroup(); ?>
-		<?php if ($group !== null && $group->getId() !== null) echo Form::hidden("id_group", $group->getId()); ?>
+<div class="row g-2">
+	<?php $group = $subject->getGroup(); ?>
+	<?php if ($group !== null && $group->getId() !== null) echo Form::hidden("id_group", $group->getId()); ?>
 
-		<!-- NMI -->
-		<div class="col-md-6">
-			<div class="form-floating">
-				<input name="NMI" id="form_NMI" value="<?= $group !== null ? $group->getNMI() : "" ?>"
-					type="number" class="form-control" placeholder="NMI" min="0"
-					title="Indiquez le Nombre Minimum d'Individu que compose le groupe dont fait parti le sujet handicapé.">
-				<label for="form_NMI">NMI</label>
-			</div>
-		</div>
-
-		<!-- Chronologie -->
-		<?php $chrono = $group !== null && $group->getChronology() !== null ? $group->getChronology()->getId() : 18; ?>
-		<div class="col-md-6">
-			<div class="form-floating">
-				<select name="id_chronologie" id="form_id_chronologie" class="form-select"
-					title="Indiquer la phase chronologique à laquelle appartient le sujet handicapé">
-					<?= Chronology::fetchOptions($chrono) ?>
-				</select>
-				<label for="form_id_chronologie">Chronologie</label>
-			</div>
+	<!-- NMI -->
+	<div class="col-md-6">
+		<div class="form-floating">
+			<input name="NMI" id="form_NMI" value="<?= $group !== null ? $group->getNMI() : "" ?>"
+				type="number" class="form-control" placeholder="NMI" min="0"
+				title="Indiquez le Nombre Minimum d'Individu que compose le groupe dont fait parti le sujet handicapé.">
+			<div class="form-msg-error">La valeur doit être un nombre positif.</div>
+			<label for="form_NMI">NMI</label>
 		</div>
 	</div>
 
-	<h3 class="text-center my-2">Sujet handicapé</h3>
-	<div class="row g-2">
-
-		<div class="row g-2">
-			<!-- Identifiant -->
-			<div class="col-md-6">
-				<div class="form-floating">
-					<input name="id_sujet_handicape" id="form_id_sujet_handicape" value="<?= $subject->getIdSujetHandicape(); ?>"
-						type="text" class="form-control" placeholder="" maxlength="256"
-						title="Indiquez le numéro d'enregistrement du sujet">
-					<label for="form_id_sujet_handicape">Identifiant du sujet</label>
-				</div>
-			</div>
-
-			<!-- Sexe -->
-			<div class="col-md-6">
-				<div class="form-floating">
-					<?= Sex::generateSelect("sexe", $subject->getSexe()); ?>
-				</div>
-			</div>
+	<!-- Chronologie -->
+	<?php $chrono = $group !== null && $group->getChronology() !== null ? $group->getChronology()->getId() : 18; ?>
+	<div class="col-md-6">
+		<div class="form-floating">
+			<select name="id_chronologie" id="form_id_chronologie" class="form-select" required
+				title="Indiquer la phase chronologique à laquelle appartient le sujet handicapé">
+				<?= Chronology::fetchOptions($chrono) ?>
+			</select>
+			<label for="form_id_chronologie">Chronologie</label>
 		</div>
+	</div>
+</div>
 
-		<div class="row g-2">
-			<!-- Âge minimum estimé de décès -->
-			<div class="col-md-6">
-				<div class="form-floating">
-					<input name="age_min" id="form_age_min" value="<?= $subject->getAgeMin(); ?>"
-						type="number" class="form-control" placeholder="Âge minimum au décès" min="0" max="130" step="1"
-						title="Indiquez l'âge minimum estimé du sujet">
-					<label for="form_age_min">Âge minimum au décès</label>
-				</div>
-			</div>
+<h3 class="text-center my-2">Sujet handicapé</h3>
 
-			<!-- Âge maximum estimé de décès -->
-			<div class="col-md-6">
-				<div class="form-floating">
-					<input name="age_max" id="form_age_max" value="<?= $subject->getAgeMax(); ?>"
-						type="number" class="form-control" placeholder="Âge maximum au décès" min="0" max="130" step="1"
-						title="Indiquez l'âge maximum estimé du sujet">
-					<label for="form_age_max">Âge maximum au décès</label>
-				</div>
-			</div>
-		</div>
-
-		<div class="row g-2">
-			<!-- Période minimum estimé -->
-			<div class="col-md-6">
-				<div class="form-floating">
-					<input name="dating_min" id="form_dating_min" value="<?= $subject->getDatingMin(); ?>"
-						type="number" class="form-control" placeholder="Datation minimale" min="-20000" max="1945" step="1"
-						title="Indiquez la borne inférieure de datation du sujet">
-					<label for="form_dating_min">Datation minimale</label>
-				</div>
-			</div>
-
-			<!-- Période maximum estimé -->
-			<div class="col-md-6">
-				<div class="form-floating">
-					<input name="dating_max" id="form_dating_max" value="<?= $subject->getDatingMax(); ?>"
-						type="number" class="form-control" placeholder="Datation maximal" min="-20000" max="1945" step="1"
-						title="Indiquez la borne supérieure de datation du sujet">
-					<label for="form_dating_max">Datation maximale</label>
-				</div>
-			</div>
-
+<div class="row my-4">
+	<!-- Identifiant -->
+	<div class="col-md-6">
+		<div class="form-floating">
+			<input name="id_sujet_handicape" id="form_id_sujet_handicape" value="<?= $subject->getIdSujetHandicape(); ?>"
+				type="text" class="form-control" placeholder="" maxlength="256"
+				onclick="this.onkeyup=function () { this.required = `required`; }"
+				title="Indiquez le numéro d'enregistrement du sujet">
+			<div class="form-msg-error">Veuillez indiquer une valeur pour ce champ.</div>
+			<label for="form_id_sujet_handicape">Identifiant du sujet</label>
 		</div>
 	</div>
 
-	<div class="row my-3">
-		<!-- Type de dépôt -->
-		<div class="col-md-4">
-			<div class="form-floating">
-				<select name="id_type_depot" id="form_id_type_depot" class="form-select"
-					title="Indiquez la modalité du dépôt">
-					<?= Typedepot::fetchOptions($subject->getIdTypeDepot()) ?>
-				</select>
-				<label for="form_id_type_depot">Type de dépôt</label>
-			</div>
+	<!-- Sexe -->
+	<div class="col-md-6">
+		<div class="form-floating">
+			<?= Sex::generateSelect("sexe", $subject->getSexe()); ?>
 		</div>
+	</div>
+</div>
 
-		<!-- Type de sepulture -->
-		<div class="col-md-4">
-			<div class="form-floating">
-				<select name="id_sepulture" id="form_id_sepulture" class="form-select"
-					title="Indiquez le type de la sépulture">
-					<?= Typesepulture::fetchOptions($subject->getIdTypeSepulture()) ?>
-				</select>
-				<label for="form_id_sepulture">Type de sépulture</label>
-			</div>
-		</div>
-
-		<!-- Contexte normatif -->
-		<div class="col-md-4">
-			<div class="form-floating">
-				<?=
-				Form::select(
-					"contexte_normatif",
-					$subject->getContexteNormatif(),
-					array(
-						"" => "Indéterminé",
-						"Standard" => "Standard",
-						"Atypique" => "Atypique"
-					),
-					array(
-						"class" => "form-select",
-						"title" => "Indiquer le type de contexte."
-					)
-				);
-				?>
-				<label for="form_contexte_normatif">Contexte normatif</label>
-			</div>
+<div class="row my-4">
+	<!-- Âge minimum estimé de décès -->
+	<div class="col-md-6">
+		<div class="form-floating">
+			<input name="age_min" id="form_age_min" value="<?= $subject->getAgeMin(); ?>"
+				type="number" class="form-control" placeholder="Âge minimum au décès" min="0" max="130" step="1" required
+				title="Indiquez l'âge minimum estimé du sujet">
+			<div class="form-msg-error">La valeur doit être un nombre entier entre 0 et 130.</div>
+			<label for="form_age_min">Âge minimum au décès</label>
 		</div>
 	</div>
 
-	<div class="row g-2">
-		<!-- Milieu de vie -->
-		<div class="col-md-6">
-			<div class="form-floating">
-				<?=
-				Form::select(
-					"milieu_vie",
-					$subject->getMilieuVie(),
-					array(
-						"" => "Indéterminé",
-						"Rural" => "Rural",
-						"Urbain" => "Urbain"
-					),
-					array(
-						"class" => "form-select",
-						"title" => "Indiquez le milieu de vie de l'occupant."
-					)
-				);
-				?>
-				<?= Form::label("Milieu de vie", "milieu_vie") ?>
-			</div>
+	<!-- Âge maximum estimé de décès -->
+	<div class="col-md-6">
+		<div class="form-floating">
+			<input name="age_max" id="form_age_max" value="<?= $subject->getAgeMax(); ?>"
+				type="number" class="form-control" placeholder="Âge maximum au décès" min="0" max="130" step="1" required
+				title="Indiquez l'âge maximum estimé du sujet">
+			<div class="form-msg-error">La valeur doit être un nombre entier entre 0 et 130.</div>
+			<label for="form_age_max">Âge maximum au décès</label>
 		</div>
+	</div>
+</div>
 
-		<!-- Contexte -->
-		<div class="col-md-6">
-			<div class="form-floating">
-				<?= Form::select(
-					"contexte",
-					$subject->getContexte(),
-					array(
-						"" => "Indéterminé",
-						"Funeraire" => "Funéraire",
-						"Domestique" => "Domestique",
-						"Autre" => "Autre"
-					),
-					array(
-						"class" => "form-select",
-						"title" => "Indiquez le contexte de l'occupation."
-					)
-				);
-				?>
-				<?= Form::label("Contexte de la tombe", "contexte") ?>
-			</div>
+<div class="row my-4">
+	<!-- Période minimum estimé -->
+	<div class="col-md-6">
+		<div class="form-floating">
+			<input name="dating_min" id="form_dating_min" value="<?= $subject->getDatingMin(); ?>"
+				type="number" class="form-control" placeholder="Datation minimale" min="-20000" max="1945" step="1" required
+				title="Indiquez la borne inférieure de datation du sujet">
+			<div class="form-msg-error">La valeur doit être un nombre entier entre -20000 et 1945.</div>
+			<label for="form_dating_min">Datation minimale</label>
 		</div>
 	</div>
 
-	<!-- Commentaire contexte -->
-	<div class="col-md-12">
-		<label for="form_comment_contexte">Commentaire</label>
-		<textarea name="comment_contexte" id="form_comment_contexte"
-			class="form-control" rows="2" maxlength="65535"
-			title="Ecrivez ici des commentaires sur le groupe ou la sépulture en question si cela est nécessaire."
-		><?= $subject->getCommentContext(); ?></textarea>
-	</div>
-	<br />
-
-	<div class="row">
-		<!-- Accessoires -->
-		<div class="col-md-6">
-			<h3>Accessoires</h3>
-			<?php $subFurnituresId = $subject->getFurnituresId(); ?>
-			<?php foreach (Mobilier::fetchAll() as $mobilier) : ?>
-				<div class="form-check form-switch">
-					<label for="form_id_mobiliers_<?= $mobilier->getId() ?>" class="form-check-label"><?= $mobilier->getNom() ?></label>
-					<input name="id_mobiliers[]" id="form_id_mobiliers_<?= $mobilier->getId() ?>" value="<?= $mobilier->getId() ?>"
-						type="checkbox" class="form-check-input"
-						<?php if (in_array($mobilier->getId(), $subFurnituresId)) : ?>checked<?php endif; ?>>
-				</div>
-			<?php endforeach; ?>
-			<div>
-				<label class="form-check-label" for="form_description_mobilier">Description du mobilier</label>
-				<textarea class="form-control" name="description_mobilier" id="form_description_mobilier" rows="2"
-				><?= $subject->getDescriptionMobilier() ?></textarea>
-			</div>
+	<!-- Période maximum estimé -->
+	<div class="col-md-6">
+		<div class="form-floating">
+			<input name="dating_max" id="form_dating_max" value="<?= $subject->getDatingMax(); ?>"
+				type="number" class="form-control" placeholder="Datation maximal" min="-20000" max="1945" step="1" required
+				title="Indiquez la borne supérieure de datation du sujet">
+			<div class="form-msg-error">La valeur doit être un nombre entier entre -20000 et 1945.</div>
+			<label for="form_dating_max">Datation maximale</label>
 		</div>
+	</div>
 
-		<!-- Dépôt -->
-		<div class="col-md-6">
-			<h3>Dépôt</h3>
-			<?php
-			$depot = $subject->getDepot();
-			if ($depot !== null && $depot->getId() !== null) echo Form::hidden("id_depot", $depot->getId());
+</div>
+
+<div class="row my-3">
+	<!-- Type de dépôt -->
+	<div class="col-md-4">
+		<div class="form-floating">
+			<select name="id_type_depot" id="form_id_type_depot" class="form-select"
+				title="Indiquez la modalité du dépôt">
+				<?= Typedepot::fetchOptions($subject->getIdTypeDepot()) ?>
+			</select>
+			<label for="form_id_type_depot">Type de dépôt</label>
+		</div>
+	</div>
+
+	<!-- Type de sepulture -->
+	<div class="col-md-4">
+		<div class="form-floating">
+			<select name="id_sepulture" id="form_id_sepulture" class="form-select"
+				title="Indiquez le type de la sépulture">
+				<?= Typesepulture::fetchOptions($subject->getIdTypeSepulture()) ?>
+			</select>
+			<label for="form_id_sepulture">Type de sépulture</label>
+		</div>
+	</div>
+
+	<!-- Contexte normatif -->
+	<div class="col-md-4">
+		<div class="form-floating">
+			<?=
+			Form::select(
+				"contexte_normatif",
+				$subject->getContexteNormatif(),
+				array(
+					"" => "Indéterminé",
+					"Standard" => "Standard",
+					"Atypique" => "Atypique"
+				),
+				array(
+					"class" => "form-select",
+					"title" => "Indiquer le type de contexte."
+				)
+			);
 			?>
+			<label for="form_contexte_normatif">Contexte normatif</label>
+		</div>
+	</div>
+</div>
 
-			<!-- Numéro de dépôt -->
-			<div class="form-floating my-2">
-				<input name="num_inventaire" id="form_num_inventaire" value="<?= $depot === null ? null : $depot->getNumInventaire() ?>"
-					type="text" class="form-control" placeholder="Numéro de dépôt" maxlength="256"
-					title="Indiquez le numéro du dépôt.">
-				<label for="form_num_inventaire">Numéro de dépôt</label>
-			</div>
+<div class="row g-2">
+	<!-- Milieu de vie -->
+	<div class="col-md-6">
+		<div class="form-floating">
+			<?=
+			Form::select(
+				"milieu_vie",
+				$subject->getMilieuVie(),
+				array(
+					"" => "Indéterminé",
+					"Rural" => "Rural",
+					"Urbain" => "Urbain"
+				),
+				array(
+					"class" => "form-select",
+					"title" => "Indiquez le milieu de vie de l'occupant."
+				)
+			);
+			?>
+			<?= Form::label("Milieu de vie", "milieu_vie") ?>
+		</div>
+	</div>
 
-			<!-- Commune du dépôt -->
-			<div class="form-floating my-2">
-				<?php $communeName = $depot !== null && $depot->getCommune() !== null ? $depot->getCommune()->fullName() : null; ?>
-				<input name="depot_commune" id="form_depot_commune" value="<?= $communeName ?>"
-					type="text" class="form-control" placeholder="Commune" maxlength="256" autocomplete="off"
-					title="Indiquez la commune du dépôt du sujet.">
-				<label for="form_depot_commune">Commune</label>
-			</div>
-			<script>addAutocomplete("form_depot_commune", "commune");</script>
+	<!-- Contexte -->
+	<div class="col-md-6">
+		<div class="form-floating">
+			<?= Form::select(
+				"contexte",
+				$subject->getContexte(),
+				array(
+					"" => "Indéterminé",
+					"Funeraire" => "Funéraire",
+					"Domestique" => "Domestique",
+					"Autre" => "Autre"
+				),
+				array(
+					"class" => "form-select",
+					"title" => "Indiquez le contexte de l'occupation."
+				)
+			);
+			?>
+			<?= Form::label("Contexte de la tombe", "contexte") ?>
+		</div>
+	</div>
+</div>
 
-			<!-- Adresse du dépôt -->
-			<div class="form-floating my-2">
-				<input name="depot_adresse" id="form_depot_adresse" value="<?= $depot === null ? null : $depot->getAdresse() ?>"
-					type="text" class="form-control" placeholder="Adresse du dépôt" maxlength="256"
-					title="Indiquez l'adresse du dépôt du sujet">
-				<label for="form_depot_adresse">Adresse du dépôt</label>
+<!-- Commentaire contexte -->
+<div class="col-md-12">
+	<label for="form_comment_contexte">Commentaire</label>
+	<textarea name="comment_contexte" id="form_comment_contexte"
+		class="form-control" rows="2" maxlength="65535"
+		title="Ecrivez ici des commentaires sur le groupe ou la sépulture en question si cela est nécessaire."
+	><?= $subject->getCommentContext(); ?></textarea>
+</div>
+<br />
+
+<div class="row">
+	<!-- Accessoires -->
+	<div class="col-md-6">
+		<h3>Accessoires</h3>
+		<?php $subFurnituresId = $subject->getFurnituresId(); ?>
+		<?php foreach (Mobilier::fetchAll() as $mobilier) : ?>
+			<div class="form-check form-switch">
+				<label for="form_id_mobiliers_<?= $mobilier->getId() ?>" class="form-check-label"><?= $mobilier->getNom() ?></label>
+				<input name="id_mobiliers[]" id="form_id_mobiliers_<?= $mobilier->getId() ?>" value="<?= $mobilier->getId() ?>"
+					type="checkbox" class="form-check-input"
+					<?php if (in_array($mobilier->getId(), $subFurnituresId)) : ?>checked<?php endif; ?>>
 			</div>
+		<?php endforeach; ?>
+		<div>
+			<label class="form-check-label" for="form_description_mobilier">Description du mobilier</label>
+			<textarea class="form-control" name="description_mobilier" id="form_description_mobilier" rows="2"
+			><?= $subject->getDescriptionMobilier() ?></textarea>
+		</div>
+	</div>
+
+	<!-- Dépôt -->
+	<div class="col-md-6">
+		<h3>Dépôt</h3>
+		<?php
+		$depot = $subject->getDepot();
+		if ($depot !== null && $depot->getId() !== null) echo Form::hidden("id_depot", $depot->getId());
+		?>
+
+		<!-- Numéro de dépôt -->
+		<div class="form-floating my-2">
+			<input name="num_inventaire" id="form_num_inventaire" value="<?= $depot === null ? null : $depot->getNumInventaire() ?>"
+				type="text" class="form-control" placeholder="Numéro de dépôt" maxlength="256"
+				title="Indiquez le numéro du dépôt.">
+			<label for="form_num_inventaire">Numéro de dépôt</label>
+		</div>
+
+		<!-- Commune du dépôt -->
+		<div class="form-floating my-2">
+			<?php $communeName = $depot !== null && $depot->getCommune() !== null ? $depot->getCommune()->fullName() : null; ?>
+			<input name="depot_commune" id="form_depot_commune" value="<?= $communeName ?>"
+				type="text" class="form-control" placeholder="Commune" maxlength="256" autocomplete="off"
+				title="Indiquez la commune du dépôt du sujet.">
+			<label for="form_depot_commune">Commune</label>
+		</div>
+		<script>addAutocomplete("form_depot_commune", "commune");</script>
+
+		<!-- Adresse du dépôt -->
+		<div class="form-floating my-2">
+			<input name="depot_adresse" id="form_depot_adresse" value="<?= $depot === null ? null : $depot->getAdresse() ?>"
+				type="text" class="form-control" placeholder="Adresse du dépôt" maxlength="256"
+				title="Indiquez l'adresse du dépôt du sujet">
+			<label for="form_depot_adresse">Adresse du dépôt</label>
 		</div>
 	</div>
 </div>
