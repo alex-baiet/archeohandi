c9fcf610e851d2f834c522168e4965dcc824c9f0
alex-baiet <alex.baiet3@gmail.com>
2022-03-16 08:28:04 +0100

    Ajout modele d'export

---
+++ fuel/app/views/recherche/hidden.php
@@ -0,0 +1,23 @@
+<!-- Modele de données caché -->
+<pre style="display: none">
+[
+	{
+		"operation" : {
+			// Données opérations
+		},
+		"sujets" : [
+			{ /* Données sujets 1 */},
+			{ /* Données sujets 2 */},
+			{ /* Etc */}
+		]
+	}, {
+		"operation" : {
+			// Rebelote
+		},
+		"sujets" : [
+			{ /* Données sujets 1 */},
+			{ /* Etc */}
+		]
+	},
+]
+</pre>
