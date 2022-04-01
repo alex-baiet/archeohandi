5ae609ea5eb6ad7e8561dc93fee50c7cec163eb1
alex-baiet <alex.baiet3@gmail.com>
2022-04-01 15:18:12 +0200

    Amelioration visuel operations/table

--- fuel/app/views/operations/table.php
+++ fuel/app/views/operations/table.php
@@ -43,44 +43,56 @@ $lines = $lines;
 				<td><?= $op->getAnnee() ?></td>
 				<td><a href="/public/operations/view/<?= $op->getId() ?>">Consulter</a></td>
 			</tr>
-			<tr id="row_subjects_<?= $op->getId() ?>" style="display:none; background-color: #ccc;">
-				<td colspan="100%">
+			<tr id="row_subjects_<?= $op->getId() ?>" style="display:none;">
+				<td colspan="100%" style="
+					border-color: black;
+					border-width: 1px;
+					padding: 0;"
+				>
 
 					<?php /* Tableau des sujets */ ?>
-					<table class="table table-bordered" style="background-color: white;">
-						<thead>
-							<tr>
-								<th colspan="100%">Sujets handicapés de l'opération</th>
-							</tr>
-							<tr>
-								<th>État</th>
-								<th>Id</th>
-								<th>Nom</th>
-								<th>Sexe</th>
-								<th>Datation</th>
-								<th>Milieu de vie</th>
-								<th>Type de dépôt</th>
-								<th>Type de sépulture</th>
-								<th>Âge</th>
-								<th>Actions</th>
-							</tr>
-						</thead>
-						<tbody>
-							<?php foreach ($data->subjects as $subject) : ?>
-								<tr><?php /* Affichage d'un sujet */ ?>
-									<td><?= Archeo::getCompleteIcon($subject->getComplet()) ?></td>
-									<td><?= $subject->getId() ?></td>
-									<td><?= $subject->getIdSujetHandicape() ?></td>
-									<td><?= $subject->getSexe() ?></td>
-									<td><?= $subject->getDatingMin() . " - " . $subject->getDatingMax() ?></td>
-									<td><?= $subject->getMilieuVie() ?></td>
-									<td><?= $subject->getTypeDepot()->getNom() ?></td>
-									<td><?= $subject->getTypeSepulture()->getNom() ?></td>
-									<td><?= $subject->getAgeMin() . " - " . $subject->getAgeMax() ?></td>
-									<td><a href="/public/sujet/view/<?= $subject->getId() ?>">Consulter</a></td>
+					<table class="table-child table table-bordered">
+						<?php if (empty($data->subjects)) : ?>
+							<thead>
+								<tr>
+									<th colspan="100%">Aucun sujets handicapés</th>
 								</tr>
-							<?php endforeach ?>
-						</tbody>
+							</thead>
+						<?php else : ?>
+							<thead>
+								<tr>
+									<th colspan="100%">Sujets handicapés de l'opération</th>
+								</tr>
+								<tr>
+									<th>État</th>
+									<th>Id</th>
+									<th>Nom</th>
+									<th>Sexe</th>
+									<th>Datation</th>
+									<th>Milieu de vie</th>
+									<th>Type de dépôt</th>
+									<th>Type de sépulture</th>
+									<th>Âge</th>
+									<th>Actions</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach ($data->subjects as $subject) : ?>
+									<tr><?php /* Affichage d'un sujet */ ?>
+										<td><?= Archeo::getCompleteIcon($subject->getComplet()) ?></td>
+										<td><?= $subject->getId() ?></td>
+										<td><?= $subject->getIdSujetHandicape() ?></td>
+										<td><?= $subject->getSexe() ?></td>
+										<td><?= $subject->getDatingMin() . " - " . $subject->getDatingMax() ?></td>
+										<td><?= $subject->getMilieuVie() ?></td>
+										<td><?= $subject->getTypeDepot()->getNom() ?></td>
+										<td><?= $subject->getTypeSepulture()->getNom() ?></td>
+										<td><?= $subject->getAgeMin() . " - " . $subject->getAgeMax() ?></td>
+										<td><a href="/public/sujet/view/<?= $subject->getId() ?>">Consulter</a></td>
+									</tr>
+								<?php endforeach ?>
+							</tbody>
+						<?php endif; ?>
 					</table>
 
 				</td>
